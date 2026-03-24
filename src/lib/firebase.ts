a4d56d44dde56be5235b948fd71f4533a1d46ed0
chandan91077 <153637267+chandan91077@users.noreply.github.com>
2026-03-24 17:13:15 +0530

    fix the appkey problem

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJrYrcsFMkDCHk-7Hb3eAfIo1MzYGQ1MM",
+  apiKey: "AIzaSyCLguKmXx5Jy2k4qAV86h9N3hMoAHSKgnA",
   authDomain: "mediconnect-8536b.firebaseapp.com",
   projectId: "mediconnect-8536b",
   storageBucket: "mediconnect-8536b.firebasestorage.app",
   messagingSenderId: "686900284890",
-  appId: "1:686900284890:android:ee7a9aee288049b39984e3",
+  appId: "1:686900284890:web:f3b68528fbfc56b39984e3",
 };
 
 const app = initializeApp(firebaseConfig);
